9820ca65bdfaa0efa5fcf69489b00a99bbf9bbcc
redaphid <iam@aaronherres.com>
2022-12-13 00:37:44 -1000

    getting testing working

---
+++ test/1.cpp
@@ -0,0 +1,19 @@
+#include <unity.h>
+
+void setUp(void) {
+    // set stuff up here
+}
+
+void tearDown(void) {
+    // clean stuff up here
+}
+
+int main( int argc, char **argv) {
+    UNITY_BEGIN();
+    UNITY_END();
+}
+
+
+void test_circular_buffer_reports_full_correctly() {
+    TEST_ASSERT_TRUE(true);
+}
