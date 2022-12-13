79bfc140cbf45169298e8824cc5b6b41513bb6bf
redaphid <iam@aaronherres.com>
2022-12-13 13:50:20 -1000

    getting a handle on tests

--- test/friend-finder.cpp
+++ test/friend-finder.cpp
@@ -9,21 +9,21 @@ void setUp(void) {
 
 void tearDown(void) {
     // clean stuff up here
-    manager = NULL;
+    delete manager;
 }
 
-void test_friend_manager_exists(void) {
+void test_exists(void) {
     TEST_ASSERT_NOT_NULL(manager);
 }
-void test_friend_manager_returns_friends(void) {
+void test_returns_friends(void) {
     auto friends = manager->getFriends();
     TEST_ASSERT_NOT_NULL(friends);
 }
 void setup()
 {
     UNITY_BEGIN();
-    RUN_TEST(test_friend_manager_exists);
-    RUN_TEST(test_friend_manager_returns_friends);
+    RUN_TEST(test_exists);
+    RUN_TEST(test_returns_friends);
     UNITY_END(); // stop unit testing
 }
 
