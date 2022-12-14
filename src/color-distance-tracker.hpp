6006d8b426d00b19fa1c87940697d97ca8ce8c8f
redaphid <iam@aaronherres.com>
2022-12-14 00:07:41 -1000

    try only using the most basic, fixed-length data structures possible

--- src/color-distance-tracker.hpp
+++ src/color-distance-tracker.hpp
@@ -17,36 +17,71 @@ namespace colortracker
     uint8_t g;
     uint8_t b;
     uint8_t strength;
+    bool is_empty;
   };
 
   struct Device
   {
-    char *name;
     uint8_t id;
-    uint8_t rssi;
+    int32_t rssi;
+    bool is_empty;
   };
 
   Device devices[NUM_COLOR_TRACKERS];
   Device newDevices[NUM_COLOR_TRACKERS];
   Sense senses[NUM_COLOR_TRACKERS];
+  uint32_t num_color_trackers = NUM_COLOR_TRACKERS;
+
+  void init(Device* d){
+    for (int i = 0; i < NUM_COLOR_TRACKERS; i++)
+    {
+      d[i].id = i;
+      d[i].is_empty = true;
+    }
+  }
+  void init(Sense* s){
+    for (int i = 0; i < NUM_COLOR_TRACKERS; i++)
+    {
+      s[i].id = i;
+      s[i].is_empty = true;
+    }
+  }
+
   void init()
   {
     for (int i = 0; i < NUM_COLOR_TRACKERS; i++)
     {
+      init(devices);
+      init(newDevices);
+      init(senses);
+    }
+  }
+  void detect(Device device){
+    if (device.is_empty)
+    {
+      return;
+    }
+    if (devices[device.id].is_empty)
+    {
+      devices[device.id].rssi = 0;
+      devices[device.id].is_empty = false;
+    }
+    newDevices[device.id].rssi = device.rssi;
+    newDevices[device.id].is_empty = false;
+  };
 
-      devices[i].name = NULL;
-      devices[i].rssi = 0;
-      devices[i].id = i;
+  void update(){
+    for (int i = 0; i < NUM_COLOR_TRACKERS; i++)
+    {
+      auto device = devices[i];
+      auto sense = senses[i];
 
-      newDevices[i].name = NULL;
-      newDevices[i].rssi = 0;
-      newDevices[i].id = i;
-
-      senses[i].id = i;
-      senses[i].r = 0;
-      senses[i].g = 0;
-      senses[i].b = 0;
-      senses[i].strength = 0;
+      if(device.is_empty){
+        sense.is_empty = true;
+        continue;
+      }
+      sense.is_empty = false;
+      sense.strength = 100;
     }
   }
 }
--- test/color-tracker.cpp
+++ test/color-tracker.cpp
@@ -12,6 +12,19 @@ void test_tracker_exists(void) {
    TEST_ASSERT_EQUAL(1, sense.id);
 }
 
+void test_tracker_gives_strength(void) {
+   colortracker::detect({
+        .id = 1,
+        .rssi = -55,
+        .is_empty = false
+   });
+   colortracker::update();
+   auto sense = colortracker::senses[1];
+   auto strength = sense.strength;
+   TEST_ASSERT_FALSE(sense.is_empty);
+//    TEST_ASSERT_EQUAL(1, sense.id);
+}
+
 
 void setup()
 {
